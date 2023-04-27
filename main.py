61024d93e215f5b21a43f886fe8706532d7473e8
MelWng <73227471+MelWng@users.noreply.github.com>
2023-04-27 12:37:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+import os
+import requests, json
+
+def get_weather():
+    LAT = os.getenv('LAT')
+    LONG = os.getenv('LONG')
+    API_KEY = os.getenv('API_KEY')
+
+    if not (LAT and LONG and API_KEY):
+        return None
+    #API_KEY = "f533aa1341045b36b24d64cd972e3d4e"
+    # base URL
+    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+    # upadting the URL
+    URL = BASE_URL + "lat=" + LAT +"&lon=" + LONG + "&appid=" + API_KEY
+    # HTTP request
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        return weather_data
+    else:
+        return None
+
+if __name__ == '__main__':
+    response = get_weather()
+    print(response)
