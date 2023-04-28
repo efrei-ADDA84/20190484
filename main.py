a2732f2e24c3d5d06424cf2beae324a9b2bcee35
melin <melina.wong@efrei.net>
2023-04-28 12:11:45 +0200

    CI/CD

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import os
 import requests
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
 def get_weather():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
-    API_KEY = os.getenv('API_KEY')
+    API_KEY = os.environ.get('API_KEY')
 
-    if not (lat and long and API_KEY):
+    if not (lat and lon and API_KEY):
         return None
-    
+
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-  
+
     #URL
     URL = BASE_URL + "lat=" + lat +"&lon=" + lon + "&appid=" + API_KEY
     
@@ -23,7 +25,7 @@ def get_weather():
         weather_data = response.json()
         return weather_data
     else:
-        return None
+        return "Error"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
